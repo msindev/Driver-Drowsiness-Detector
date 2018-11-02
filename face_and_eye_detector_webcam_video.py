2c8b05335614776a04040dd9b20d1cbb25915282
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-02 23:46:12 +0530

    Working code for webcam face and eye detection code.

---
+++ face_and_eye_detector_webcam_video.py
@@ -0,0 +1,36 @@
+'''This script uses OpenCV's haarcascade (face and eye cascade) to detect face
+and eyes in a video feed which can be inputted through a webcam.'''
+
+import cv2 as cv
+import numpy as np
+
+face_cascade = cv.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+eye_cascade = cv.CascadeClassifier("haarcascades/haarcascade_eye.xml")
+
+video_capture = cv.VideoCapture(0) #Capture video from webcam
+
+while True:
+    ret, frame = video_capture.read()
+    frame = cv.flip(frame,1)
+    gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+
+    faces = face_cascade.detectMultiScale(gray, 1.3, 5)
+
+    for (x,y,w,h) in faces:
+        cv.rectangle(frame,(x,y),(x+w,y+h),(255,0,0),2)
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = frame[y:y+h, x:x+w]
+
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+
+        for (ex,ey,ew,eh) in eyes:
+            cv.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,0),2)
+
+    cv.imshow('Video', frame)
+
+    if(cv.waitKey(1) & 0xFF == ord('q')):
+        break
+
+#Finally when video capture is over, release the video capture and destroyAllWindows
+video_capture.release()
+cv.destroyAllWindows()
