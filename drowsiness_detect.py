1c3a257bd1673bbada06b3bf4ae59437b1734a99
Mohit Singh <mohitwildbeast@gmail.com>
2018-11-03 17:55:04 +0530

    Detecting drowsiness through eye aspect ratio working.

---
+++ drowsiness_detect.py
@@ -0,0 +1,69 @@
+from scipy.spatial import distance
+from imutils import face_utils
+from threading import Thread
+import numpy as np
+#Insert sound library functions here
+import imutils
+import time
+import dlib
+import cv2
+
+def eye_aspect_ratio(eye):
+    A = distance.euclidean(eye[1], eye[5])
+    B = distance.euclidean(eye[2], eye[4])
+    C = distance.euclidean(eye[0], eye[3])
+
+    ear = (A+B) / (2*C)
+    return ear
+
+EYE_ASPECT_RATIO_THRESHOLD = 0.3
+EYE_ASPECT_RATIO_CONSEC_FRAMES = 50
+
+COUNTER = 0
+
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor('shape_predictor_68_face_landmarks.dat')
+
+#Extract indexes of facial landmarks for the left and right eye
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_IDXS['left_eye']
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_IDXS['right_eye']
+
+video_capture = cv2.VideoCapture(0)
+time.sleep(2)
+
+while(True):
+    ret, frame = video_capture.read()
+    frame = cv2.flip(frame,1)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    faces = detector(gray, 0)
+    for face in faces:
+        shape = predictor(gray, face)
+        shape = face_utils.shape_to_np(shape)
+
+        leftEye = shape[lStart:lEnd]
+        rightEye = shape[rStart:rEnd]
+        leftEyeAspectRatio = eye_aspect_ratio(leftEye)
+        rightEyeAspectRatio = eye_aspect_ratio(rightEye)
+
+        eyeAspectRatio = (leftEyeAspectRatio + rightEyeAspectRatio) / 2
+
+        leftEyeHull = cv2.convexHull(leftEye)
+        rightEyeHull = cv2.convexHull(rightEye)
+        cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+        cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+
+        if(eyeAspectRatio < EYE_ASPECT_RATIO_THRESHOLD):
+            COUNTER += 1
+            if COUNTER >= EYE_ASPECT_RATIO_CONSEC_FRAMES:
+                cv2.putText(frame, "You are Drowsy", (10,30), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0,0,255), 2)
+        else:
+            COUNTER = 0
+
+    cv2.imshow('Video', frame)
+    if(cv2.waitKey(1) & 0xFF == ord('q')):
+        break
+
+#Finally when video capture is over, release the video capture and destroyAllWindows
+video_capture.release()
+cv.destroyAllWindows()
